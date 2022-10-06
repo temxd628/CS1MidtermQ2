214103e1773081d051b02db932ddf6d405d662dc
Kyuwoong Lee <kwooler@gmail.com>
2022-10-06 01:20:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
-
+#include <fstream>
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	const int N = 5, M = 5;
-	int i, j;
-
-	for (/* Complete this condition */ )
-	{
-		for (/* Complete this condition */ )
-			cout << " (" << i << "," << j << ") ";
-		cout << endl;
-	}
-	return 0;
+	// *************************
+      	// Complete Your Code
+	// *************************
 }
