2854b9c63ae89a8fb9962ad3cea065a22d2c7345
Tem <111550227+temxd628@users.noreply.github.com>
2022-10-13 20:14:10 -0700

    Q2.cpp
    students name and 2 scores

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main()
-{
-	// *************************
-      	// Complete Your Code
-	// *************************
+
+int main() {
+    ifstream fh;
+    fh.open("students.txt");
+    if (!fh) {
+        cerr << "unable to open file\n";
+        return 1;
+    }
+    string name;
+    int grade1, grade2;
+    while (fh >> name >> grade1 >> grade2) {
+        if ((grade1 + grade2) / 2. > 80.) {
+            cout << name << endl;
+        }
+    }
+    return 0;
 }
